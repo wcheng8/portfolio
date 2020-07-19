1334782c8aea01af49435dee5b619d95917320a4
Wilkins Cheng <wilkinscheng@MacBook-Pro.hub>
2020-07-19 11:42:10 +1000

    Updates

---
+++ component---src-pages-index-js-7297c78b1902e308387b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{RXBc:function(e,t,n){"use strict";n.r(t),n.d(t,"default",(function(){return o}));var a=n("q1tI"),l=n.n(a);function o(){return l.a.createElement("div",{style:{color:"purple"}},l.a.createElement("h1",null,"Hello Gatsby!booo"),l.a.createElement("p",null,"What a world."),l.a.createElement("img",{src:"https://source.unsplash.com/random/400x200",alt:""}))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-7297c78b1902e308387b.js.map
